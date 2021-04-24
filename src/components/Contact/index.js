d8fe0dd00ab6fd3e439c40ab05ef75f9ab3a19c2
noplur <geoduckphoto@gmail.com>
2021-04-24 14:31:19 -0700

    complete contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
 
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-
+    const [errorMessage, setErrorMessage] = useState('');
     const { name, email, message } = formState;
 
     function handleSubmit(e) {
@@ -12,8 +13,26 @@ function ContactForm() {
       }
 
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
-      }
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+              setErrorMessage(`${e.target.name} is required.`);
+            } else {
+              setErrorMessage('');
+            }
+          }
+          if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+          }
+    }
       
 
 return (
@@ -32,6 +51,11 @@ return (
             <label htmlFor="message">Message:</label>
             <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
+        {errorMessage && (
+            <div>
+                <p className="error-text">{errorMessage}</p>
+            </div>
+            )}
         <button type="submit">Submit</button>
         </form>
     </section>
