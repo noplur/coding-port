cec0cc26f79aa39d46f34cef820a39803f6ba6c9
noplur <geoduckphoto@gmail.com>
2021-04-25 20:50:23 -0700

    updated about css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import aboutImage from "../../assets/images/about/aaron.jpg";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./about.css"
 
 function About() {
     return (
         <section>
-            <h1 id="about">About me</h1>
-            <img src={aboutImage} className="my-2" style={{ width: "20%" }} alt="aaron" />
+            <h1 class="about">About Me</h1>
+            <img src={aboutImage} className="aboutimg" style={{ width: "20%" }} alt="aaron" />
             <div className="my-2">
-            <p>
+            <p class="abouttext">
                 Full stack web developer leveraging background in photojournalism to provide unique perspectives on how end-users interact with websites and software platforms. Earned a certificate in Full Stack Web Development from the Berkeley Coding Boot Camp. Innovative problem-solver who is passionate about developing apps. Strengths in creativity, teamwork, meeting deadlines and building projects from visualization to execution.
             </p>
             </div>
