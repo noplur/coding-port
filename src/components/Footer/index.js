1ee7ab7dc7a7eb9469453a42ad6d3ec0437c6e7b
noplur <geoduckphoto@gmail.com>
2021-04-25 20:36:59 -0700

    footer css

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,23 +1,25 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./footer.css"
 
 function Footer() {
     return (
         <footer>
             <nav>
-            <ul className="flex-row">
-              <li className="mx-2">
+            <ul className="footer">
+              <li className="footerlink">
                 <a href="https://github.com/noplur" target="_blank">
-                    <i class="fab fa-github"></i>
+                    <i class="fab fa-github icon"></i>
                 </a>
             </li>
-            <li className={"mx-2"}>
+            <li className="footerlink">
                 <a href="https://www.linkedin.com/in/aaron-rosenblatt-5b734a1bb/" target="_blank">
-                    <i class="fab fa-linkedin"></i>
+                    <i class="fab fa-linkedin icon"></i>
                 </a>
             </li>
-            <li className={"mx-2"}>
+            <li className="footerlink">
                 <a href="https://twitter.com/aaronpix" target="_blank">
-                    <i class="fab fa-twitter-square"></i>
+                    <i class="fab fa-twitter-square icon"></i>
                 </a>
             </li>
             </ul>
