fde9005dd81453c889ad6693601d53b0e6e2bafd
noplur <geoduckphoto@gmail.com>
2021-08-16 16:23:10 -0700

    update to mobile ready and resume pdf file

--- src/components/ProjectList/index.js
+++ src/components/ProjectList/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Card} from "react-bootstrap";
-import "./projectList.css"
+import { Card, Button } from "react-bootstrap";
+import "./projectList.css";
 
 const ProjectList = ({ category }) => {
 
@@ -10,77 +10,96 @@ const ProjectList = ({ category }) => {
       image: require(`../../assets/images/project/0.jpg`).default,
       name: 'Kathryn Hansen Art Portfolio',
       category: 'project',
-      description: 'GraphQL, React, MongoDB',
+      description: 'A group-built website for a local artist to reach out to potential customers and sales.',
+      component: 'MongoDb, React, GraphQL',
       projectlink: 'https://kathryn-hansen.herokuapp.com/',
       githublink: 'https://github.com/noplur/kathryn-hansen'
-  },
-    {
-        image: require(`../../assets/images/project/1.jpg`).default,
-        name: 'Bart Community',
-        category: 'project',
-        description: 'Express-Handlebars',
-        projectlink: 'https://bort-community.herokuapp.com/',
-        githublink: 'https://github.com/noplur/Bort-Community'
     },
     {
-        image: require(`../../assets/images/project/2.jpg`).default,
-        name: 'Musicana',
-        category: 'project',
-        description: 'JavaScript/jQuery/Foundation',
-        projectlink: 'https://noplur.github.io/team4-10-music/',
-        githublink: 'https://github.com/noplur/team4-10-music'
+      image: require(`../../assets/images/project/1.jpg`).default,
+      name: 'Bart Community',
+      category: 'project',
+      description: 'A group-built app to ease navigation through the BART public transportation system following MVC paradigm.',
+      component: 'Express-Handlebars, Sequelize, Express-Session, crypt, Dotenv, Axios',
+      projectlink: 'https://bort-community.herokuapp.com/',
+      githublink: 'https://github.com/noplur/Bort-Community'
     },
     {
-        image: require(`../../assets/images/project/3.jpg`).default,
-        name: 'High-Tek Blog',
-        category: 'project',
-        description: 'MVC/Handlebars.js/Express-Session',
-        projectlink: 'https://rosenblatt-hi-tek-blog.herokuapp.com/',
-        githublink: 'https://github.com/noplur/re-markable-high-tech-blog'
+      image: require(`../../assets/images/project/2.jpg`).default,
+      name: 'Musicana',
+      category: 'project',
+      description: 'A group-built app that allows users to discover new music using JavaScript, jQuery, Foundation and the Deezer API library.',
+      component: 'JavaScript/jQuery/Foundation',
+      projectlink: 'https://noplur.github.io/team4-10-music/',
+      githublink: 'https://github.com/noplur/team4-10-music'
     },
     {
-        image: require(`../../assets/images/project/4.jpg`).default,
-        name: 'Weather Dashboard',
-        category: 'project',
-        description: 'JavaScript/jQuery/Bootstrap',
-        projectlink: 'https://noplur.github.io/weather-dashboard/',
-        githublink: 'https://github.com/noplur/weather-dashboard'
+      // image: require(`../../assets/images/project/3.jpg`).default,
+      // name: 'High-Tek Blog',
+      // category: 'project',
+      // description: 'A group-built app that allows users to discover new music using JavaScript, jQuery, Foundation and the Deezer API library.',
+      // component: 'MVC/Handlebars.js/Express-Session',
+      // projectlink: 'https://rosenblatt-hi-tek-blog.herokuapp.com/',
+      // githublink: 'https://github.com/noplur/re-markable-high-tech-blog'
+
+      image: require(`../../assets/images/project/3.jpg`).default,
+      name: 'Portfolio',
+      category: 'project',
+      description: 'A portfolio website showing links to samples about my selected projects, information about myself and my skills using React. This is the site you are looking at now.',
+      component: 'React, JavaScript',
+      projectlink: 'https://noplur.github.io/coding-port/',
+      githublink: 'https://github.com/noplur/coding-port'
     },
     {
-        image: require(`../../assets/images/project/5.jpg`).default,
-        name: 'On-Point Note Taking',
-        category: 'project',
-        description: 'Express.js',
-        projectlink: 'https://rosenblatt-onpoint-notetaking.herokuapp.com/',
-        githublink: 'https://github.com/noplur/onPoint-Note-Taking'
+      image: require(`../../assets/images/project/4.jpg`).default,
+      name: 'Weather Dashboard',
+      category: 'project',
+      description: "Weather dashboard that incorporates third-party APIs using jQuery to show a city's current temperature, humidity, wind speed, uv index and 5-day forecast. Uses Local Storage for persistent data.",
+      component: 'JavaScript/jQuery/Bootstrap',
+      projectlink: 'https://noplur.github.io/weather-dashboard/',
+      githublink: 'https://github.com/noplur/weather-dashboard'
+    },
+    {
+      image: require(`../../assets/images/project/5.jpg`).default,
+      name: 'Google Book Search',
+      category: 'project',
+      description: "A full stack application that uses the Google Books API and allows for a user to search for books, view books, and save or delete them from the database.",
+      component: 'MongoDB, Express, React, Node',
+      projectlink: 'https://blatt-booksearch.herokuapp.com/',
+      githublink: 'https://github.com/noplur/blatt-book-search'
     }
-]);
+  ]);
 
-const renderCard = (card, index) => {
-  return (
-  
-    <Card style={{ width: '18rem' }} key={index} className="box">
-          <Card.Img variant="top" src="holder.js/100px180" src={card.image} />
-          <Card.ImgOverlay className="csstest">
-            <div className="links">
-    <a href={card.projectlink} target="_blank">
-    <Card.Title><div className="projectlink">{card.name}</div></Card.Title></a>
-      <a href={card.githublink} target="_blank"><Card.Text>
-        <div className="githubcard">
-      <i class="fab fa-github"></i></div>
-      </Card.Text></a>
-      <Card.Text><div className="projectdescription">
-      {card.description}</div>
-    </Card.Text>
-        </div>
-    </Card.ImgOverlay>
-    </Card>
-  )
-}
+  const renderCard = (card, index) => {
+    return (
 
-    return <div className="grid"> {photos.map(renderCard)}</div>
-    
-    
+      <Card style={{ width: '18rem' }} key={index} className="box">
+        <Card.Img variant="top" src="holder.js/80px180" src={card.image} />
+        <Card.ImgOverlay className="csstest">
+          <div className="links">
+            <Card.Title><div className="projecttitle">{card.name}</div></Card.Title>
+            <Card.Text><div className="projectdescription">
+              {card.description}</div>
+            </Card.Text>
+            <Card.Text><div className="projectcomponent">
+              {card.component}</div>
+            </Card.Text>
+          </div>
+          <div>
+            <a href={card.projectlink} target="_blank"><Button className="appbtn">View App</Button></a>
+            <a href={card.githublink} target="_blank"><Button className="codebtn">View Code<i class="fab fa-github"></i></Button></a>
+          </div>
+        </Card.ImgOverlay>
+      </Card>
+
+    )
+  }
+
+  return <div className="grid"> {photos.map(renderCard)}</div>
+
+
 };
+
+
 //test
 export default ProjectList;
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import pdf from "../../assets/images/resume/resume_rosenblatt.pdf"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import "./resume.css"
-
+import "./resume.css";
+import { DiHtml5, DiCss3, DiJavascript1, DiJqueryLogo, DiReact, DiMongodb, DiMysql, DiNodejsSmall, DiGit, DiAws, DiBootstrap, DiGithub, DiVisualstudio, DiNpm } from "react-icons/di";
+import { GrGraphQl, GrHeroku } from "react-icons/gr"; 
+import { SiSlack, SiJest } from "react-icons/si";
+import { Container, Row, Col } from "react-bootstrap";
 
 function Resume() {
     return (
@@ -11,17 +14,38 @@ function Resume() {
                 <h2 className="download">
                 Download my <a href = {pdf} target = "_blank" className="resume-link">Resume</a>
                 </h2>
-                <h3 className="frontend">Front-end Skills</h3>
-                    <p1 className="list-skill">* HTML</p1><br />
-                    <p2 className="list-skill">* CSS</p2><br />
-                    <p3 className="list-skill">* JavaScript</p3><br />
-                    <p4 className="list-skill">* jQuery</p4><br />
-                    <p5 className="list-skill">* ReactJS</p5>
-                <h3 className="backend">Back-end Skills</h3>
-                    <p1 className="list-skill">* MongoDB</p1><br />
-                    <p2 className="list-skill">* MySQL</p2><br />
+                <h3 className="frontend">Skills</h3>
+                <Container>
+                <Row className="row">
+            <Col xs={6} md={4}>
+        
+                    <p1 className="list-skill">* HTML</p1><i className="icon-skill"><DiHtml5 /></i><br />
+                    <p2 className="list-skill">* CSS</p2><i className="icon-skill"><DiCss3 /></i><br />
+                    <p3 className="list-skill">* JavaScript</p3><i className="icon-skill"><DiJavascript1 /></i><br />
+                    <p4 className="list-skill">* GraphQL</p4><i className="icon-skill"><GrGraphQl /></i><br /> 
+                    <p4 className="list-skill">* jQuery</p4><i className="icon-skill"><DiJqueryLogo /></i><br />
+                    <p5 className="list-skill">* ReactJS</p5><i className="icon-skill"><DiReact /></i><br />
                     <p3 className="list-skill">* Express</p3><br />
-                    <p4 className="list-skill">* Node Js</p4><br />             
+                    </Col>
+                    <Col xs={6} md={4}>
+                    <p3 className="list-skill">* Handlebars.js</p3><br />                
+                    <p4 className="list-skill">* Node Js</p4><i className="icon-skill"><DiNodejsSmall /></i><br /> 
+                    <p1 className="list-skill">* MongoDB</p1><i className="icon-skill"><DiMongodb /></i><br />
+                    <p2 className="list-skill">* MySQL</p2><i className="icon-skill"><DiMysql /></i><br />
+                    <p4 className="list-skill">* Git</p4><i className="icon-skill"><DiGit /></i><br /> 
+                    <p4 className="list-skill">* AWS</p4><i className="icon-skill"><DiAws /></i><br />                
+                    <p4 className="list-skill">* Heroku</p4><i className="icon-skill"><GrHeroku /></i><br />  
+                    </Col>
+                    <Col xs={6} md={4}>
+                    <p4 className="list-skill">* Bootstrap</p4><i className="icon-skill"><DiBootstrap /></i><br /> 
+                    <p4 className="list-skill">* Github</p4><i className="icon-skill"><DiGithub /></i><br />
+                    <p4 className="list-skill">* Slack</p4><i className="icon-skill"><SiSlack /></i><br /> 
+                    <p4 className="list-skill">* Jest</p4><i className="icon-skill"><SiJest /></i><br />
+                    <p4 className="list-skill">* Visual Studio</p4><i className="icon-skill"><DiVisualstudio /></i><br /> 
+                    <p4 className="list-skill">* NPM</p4><i className="icon-skill"><DiNpm /></i><br />  
+                    </Col>
+                    </Row>
+                    </Container>   
                 </section>
     );
 }
